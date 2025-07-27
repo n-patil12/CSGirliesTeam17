9c3e61dbe7d198df3f66dfa2ae95ad38facb69db
n-patil12 <nihapatil38@gmail.com>
2025-07-26 20:24:50 -0500

    created ai chatbot

--- moodtunes/src/components/signInPage.jsx
+++ moodtunes/src/components/signInPage.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import fbimg from '../assets/facebook-img.png';
 import youtubeimg from '../assets/youtube-img.png'
 import linkedinimg from '../assets/linkedin-img.png';
+import ChatbotIcon from './ChatbotIcon';
 
 export default function signInPage() {
   return (
@@ -37,6 +38,40 @@ export default function signInPage() {
         <button className="signup-button" type="submit">sign-up</button>
       </section>
 
+      <div className="chatbot-popup">
+
+        {/*Chat Header*/}
+        <div className="chat-header">
+          <div className="header-info">
+            <ChatbotIcon />
+            <h2 className="chatbot-logo-txt">Chatbot</h2>
+            <button class="material-symbols-rounded">keyboard_arrow_down</button>
+          </div>
+          {/*Chat Body*/}
+          <div className="chat-body">
+            <div className="message bot-message">
+              <ChatbotIcon />
+              <p className="message-text">
+                Hey there <br/> How can I help you today?
+              </p>
+            </div>
+
+            <div className="message user-message">
+              <p className="message-text">
+                blah blah blah im the user
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/*Chat Footer*/}
+        <div className="chat-footer">
+          <form action="#" className="chat-form">
+            <input type="text" placeholder="Message..." className="message-input" required/>
+            <button class="material-symbols-rounded">keyboard_arrow_up</button>
+          </form>
+        </div>
+      </div>
 
     </div>
   );
